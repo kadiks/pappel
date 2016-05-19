588d2041dd68268a954c6898d5abda9c07ad68ca
Jénaïc Cambré <jenaic@lilbigmen.com>
2016-05-19 17:48:13 +0200

    remove unused class

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ module.exports = {
   Converter: {
     XLS2Pappel: require('./build/converters/XLSX2Pappel'),
     Pappel2AndroidXML: require('./build/converters/Pappel2AndroidXML'),
-    Pappel2Strings: require('./build/converters/Pappel2Strings'),
-    Pappel2ReactNativeLocalization: require('./build/converters/Pappel2ReactNativeLocalization')
+    Pappel2Strings: require('./build/converters/Pappel2Strings')
+      /*,
+          Pappel2ReactNativeLocalization: require('./build/converters/Pappel2ReactNativeLocalization')*/
   },
   Keys: require('./build/Keys')
 };
